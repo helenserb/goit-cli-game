9d5782450566127eddb74feadbc2f44d9c15fef4
helenserb <serbinenkoifbb@gmail.com>
2023-05-23 22:00:49 -0400

    Update index.js
    init logic(question, counter,  validation, close)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const readline = require('readline');
 const fs = require('fs/promises');
 const { program } = require('commander');
 const { read } = require('fs');
+const { connected } = require('process');
 require('colors');
 
 const rl = readline.createInterface({
@@ -9,6 +10,26 @@ const rl = readline.createInterface({
     output: process.stdout,
 })
 
-rl.on('line', (txt) =>{
-console.log(`You have entered ${txt}!`)
-})
+program.option('-f --file [type]', 'file for saving game results', 'game_results.txt');
+program.parse(process.argv);
+
+let counter = 0;
+const mind = Math.ceil(Math.random() * 10);
+// const logFile = program.opts().file;
+
+const game = () => {
+    rl.question('Please enter whole number between 1 and 10:\n', (val) => {
+        const number = +val;
+        counter++;
+
+        if (number !== mind) {
+            console.log('Oh no! Try again'.red);
+            return game();
+        }
+
+        console.log(`Congratulations! You guessed in ${counter} steps!`.green);
+        rl.close();
+    })
+}
+
+game();
