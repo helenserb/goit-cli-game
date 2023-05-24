1185628ff52ead7307d1223a8ebe48ebbc068a62
helenserb <serbinenkoifbb@gmail.com>
2023-05-23 22:14:09 -0400

    Update index.js
    add additional validationfor number

--- index.js
+++ index.js
@@ -16,11 +16,20 @@ program.parse(process.argv);
 let counter = 0;
 const mind = Math.ceil(Math.random() * 10);
 // const logFile = program.opts().file;
+const isValid = (val) => {
+    if (!Number.isNaN(val) && val > 0 && val <= 10) return true;
 
+    if(Number.isNaN(val)) console.log('Please enter a number!'.yellow)
+    if (val < 1 || val > 10) console.log('Number should be between 1 and 10'.yellow);
+    return false;
+}
 const game = () => {
-    rl.question('Please enter whole number between 1 and 10:\n', (val) => {
+    rl.question('Please enter any whole number between 1 and 10:\n', (val) => {
         const number = +val;
         counter++;
+
+        if (!isValid(number)) return game();
+
 
         if (number !== mind) {
             console.log('Oh no! Try again'.red);
